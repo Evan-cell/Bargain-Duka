cb087d834f7e75d61cf4b40da3111ad4a09b9b9f
evan <malcomchege0582@gmail.com>
2023-12-05 17:46:17 +0300

    cart

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
-import { MagnifyingGlassIcon,Bars3Icon, ShoppingCartIcon} from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon,Bars3Icon, ShoppingCartIcon,ShoppingBagIcon} from '@heroicons/react/24/solid'
+import { } from '@heroicons/react/24/outline'
 
 function Header() {
   return (
@@ -20,6 +21,23 @@ function Header() {
         <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-emerald-600 hover:bg-emerald-500'>
         <input type="text" className='p-2 h-full w-6 flex-grow rounded-l-md flex-shrink focus:outline-none px-4 '/>
         <MagnifyingGlassIcon className='h-12 p-4 text-white'/>
+        </div>
+        {/* right */}
+        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='cursor-pointer hover:underline'>
+            <p>Hello Evan</p>
+            <p className='font-extrabold md:text-sm'>Account & Lists</p>
+          </div>
+          <div className='cursor-pointer hover:underline'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>& Orders</p>
+          </div>
+          <div className='relaative flex items-center cursor-pointer hover:underline'>
+            <span className='absolute top-4 right-4 md:top-3 md:right-16  h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>4</span>
+            <ShoppingCartIcon className='h-10'/>
+            <p className='hidden md:inline mt-2 font-extrabold md:text-sm'>Basket</p>
+          </div>
+          
         </div>
       </div>
       {/* bottom nav */}
