241241a24df6cc20a1d731536e68dbe688c11f75
evan <malcomchege0582@gmail.com>
2023-12-05 16:38:03 +0300

    config

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,22 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      {/* top nav */}
+      <div>
+        <div>
+          <Image
+          src="https://rb.gy/rvdflm"
+          width={150}
+          height={40}
+          />
+        </div>
+      </div>
+      {/* bottom nav */}
+      <div></div>
+    </header>
   )
 }
 
