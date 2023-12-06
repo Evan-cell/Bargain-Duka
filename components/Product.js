4ca7e4ab597bdd545a3afb722a878a4356dadb55
evan <malcomchege0582@gmail.com>
2023-12-06 13:30:54 +0300

    products displayed

--- components/Product.js
+++ components/Product.js
@@ -10,34 +10,35 @@ function Product({id,title,price,description,category,image}) {
     )
     const [hasPrime] = useState(Math.random() < 0.5)
   return (
-    <div>
-        <p>{category}</p>
+    <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
+        <p className='absolute top-2 right-2 text-xm italic text-gray-400'>{category}</p>
         <Image
+        className=''
         src={image}
         height={200}
         width={200}
         objectFit='contain'
         />
-        <h4>{title}</h4>
+        <h4 className='my-3'>{title}</h4>
         <div className='flex'>
             {Array(rating).fill().map((_,i)=>(
-                <StarIcon className='h-5'/>
+                <StarIcon className='h-5 text-emerald-600'/>
             )
             )}
         
         </div>
         
-        <p>{description}</p>
-        <div>
+        <p className='text-xm my-2 line-clamp-2'>{description}</p>
+        <div className='mb-5'>
             <CurrencyFormat quantity={price} CurrencyFormat="ksh" />
         </div>
         {hasPrime && (
-            <div>
-                <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/052018/untitled-1_282.png?zBgfG0XEfdsPUq33GRuhu6udfY3Yu_rs&itok=39OQ7JCF" alt="" />
-                <p>Free next-day delivery</p>
+            <div className='flex items-center space-x-2 -mt-5'>
+                <img className='w-12' src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/052018/untitled-1_282.png?zBgfG0XEfdsPUq33GRuhu6udfY3Yu_rs&itok=39OQ7JCF" alt="" />
+                <p className='text-xm text-gray-500'>Free next-day delivery</p>
             </div>
         )}
-        <button>Add to Basket</button>
+        <button className='mt-auto button'>Add to Basket</button>
     </div>
   )
 }
--- components/ProductFeed.js
+++ components/ProductFeed.js
@@ -3,8 +3,9 @@ import Product from './Product'
 
 function ProductFeed({products}) {
   return (
-    <div>
-        {products.map(({id,title,price,description,category,image})=>(
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+       
+        {products.slice(0,4).map(({id,title,price,description,category,image})=>(
             <Product
             key={id}
             id={id}
@@ -15,6 +16,37 @@ function ProductFeed({products}) {
             image={image}
             />
         ))}
+        <img className='md:col-span-full md:mx-auto' src="https://ke.jumia.is/cms/2023/W42/SIS/PRK_SIS.jpg" alt="" />
+       
+        {products.slice(4,8).map(({id,title,price,description,category,image})=>(
+            <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            />
+        ))}
+        <img className='md:col-span-full  md:mx-auto' src="https://ke.jumia.is/cms/2023/W42/SIS/PRK_SIS.jpg" alt="" />
+
+        {products.slice(8,).map(({id,title,price,description,category,image})=>(
+            <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+            />
+        ))}
+        <img className='md:col-span-full  md:mx-auto' src="https://ke.jumia.is/cms/2023/W42/SIS/PRK_SIS.jpg" alt="" />
+
+
+       
+       
     </div>
   )
 }
