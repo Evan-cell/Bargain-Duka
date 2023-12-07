a3f633fc599a47695e70bd352486e2a40720573e
evan <malcomchege0582@gmail.com>
2023-12-07 10:26:49 +0300

    store

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ const nextConfig = {
 module.exports = {
   nextConfig,
   images: {
-    domains: ['www.svgrepo.com', 'fakestoreapi.com'],
+    domains: ['www.svgrepo.com', 'fakestoreapi.com','links.papareact.com'],
   },
 }
